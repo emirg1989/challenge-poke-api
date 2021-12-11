7e1ffe85c0c2946f57ff648a78ac0fff4c0d015a
emirg1989 <eganimi1989@gmail.com>
2021-12-11 12:15:04 -0300

    first commit con api

---
+++ services/api-services.js
@@ -0,0 +1,11 @@
+async function get(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  }
+  
+  export default async function getPokemons() {
+    const url = "https://pokeapi.co/api/v2/pokemon?limit=151";
+    return get(url);
+  }
+  
