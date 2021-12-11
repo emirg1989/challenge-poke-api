7ce31f14c1c49e48f3de4ff6d116c2f494c3720a
emirg1989 <eganimi1989@gmail.com>
2021-12-11 18:39:41 -0300

    update header

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,22 @@
 import { Grid } from '@mui/material';
+import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import CardPokemon from '../components/cardPokemon';
 import getPokemons from '../services/api-service';
 
-export default function Home() {
-  const [pokemons, setPokemons] = useState([]);
-
-  useEffect(()=>{
-    getPokemons().then((data) => {
-      console.log(data);
-      setPokemons(data);
-    });
-  }, []);
-
+function Home({pokemons}) {
   return (
     <div >
       <header>
-
+        <div className="logo_header">
+          <Image src="/logo-pokemon.png" width={150} height={50} />
+        </div>
       </header>
       <main className="body_main">
         <h1 className="title">
-          pokemon
+          
         </h1>
-        <Grid container spacing={2} columns={{ xs: 2, sm: 8, md: 12 }}>
+        <Grid container spacing={2} columns={{ xs: 4, sm: 8, md: 12 }}>
           {pokemons.map(function(pokemon, i){
               return <CardPokemon pokemon={pokemon}  key={i} />;
           })}
@@ -30,5 +24,12 @@ export default function Home() {
       </main>
       <aside></aside>
     </div>
-  )
+  )  
 }
+export async function getServerSideProps() {
+  const pokemons = await getPokemons();
+  console.log(pokemons);
+  return { props: { pokemons } }
+}
+
+export default Home;
